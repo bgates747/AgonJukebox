56a05a3ec9d02d4270d50337096d85c60fba01ca
bgates747 <brandon.gates@gmail.com>
2025-01-07 18:46:13 -0500

    non-working progress commit on prt-interrupt version

---
+++ src/asm/jukebox_prt.asm
@@ -0,0 +1,71 @@
+    assume adl=1 
+    org 0x040000 
+    jp start 
+    align 64 
+    db "MOS" 
+    db 00h 
+    db 01h 
+
+start: 
+    push af
+    push bc
+    push de
+    push ix
+    push iy
+
+    call init
+    call main
+
+exit:
+    pop iy
+    pop ix
+    pop de
+    pop bc
+    pop af
+    ld hl,0
+
+    ret
+
+; API INCLUDES
+    include "mos_api.inc"
+    include "macros.inc"
+    include "functions.inc"
+    include "arith24.inc"
+    include "maths.inc"
+    include "files.inc"
+    include "fixed168.inc"
+    include "timer.inc"
+    include "vdu.inc"
+    include "vdu_buffered_api.inc"
+    include "vdu_fonts.inc"
+    include "vdu_plot.inc"
+    include "vdu_sound.inc"
+
+; APPLICATION INCLUDES
+    include "input_prt.inc"
+    include "music.inc"
+    include "play_prt.inc"
+    include "timer_jukebox.inc"
+    include "debug.inc"
+
+; --- MAIN PROGRAM FILE ---
+init:
+; load play sample command buffers
+    call load_command_buffer
+; initialize play sample timer interrupt handler
+    call ps_prt_irq_init
+    ret
+; end init
+
+main:
+    call printNewLine
+; point to first song in the index
+    ld hl,SFX_filename_index
+    ld hl,(hl) ; pointer to first song filename
+    call play_song
+    ret
+; end main
+
+; buffer for sound data
+; (must be last so buffer doesn't overwrite other program code or data)
+song_data:
---
+++ src/asm/test_prt.asm
@@ -0,0 +1,85 @@
+    assume adl=1 
+    org 0x040000 
+    jp start 
+    align 64 
+    db "MOS" 
+    db 00h 
+    db 01h 
+
+start: 
+    push af
+    push bc
+    push de
+    push ix
+    push iy
+
+    call init
+    call main
+
+exit:
+    pop iy
+    pop ix
+    pop de
+    pop bc
+    pop af
+    ld hl,0
+
+    ret
+
+; API INCLUDES
+    include "mos_api.inc"
+    include "macros.inc"
+    include "functions.inc"
+    include "arith24.inc"
+    include "maths.inc"
+    include "files.inc"
+    include "fixed168.inc"
+    include "timer.inc"
+    include "vdu.inc"
+    include "vdu_buffered_api.inc"
+    include "vdu_fonts.inc"
+    include "vdu_plot.inc"
+    include "vdu_sound.inc"
+
+; APPLICATION INCLUDES
+    include "timer_jukebox.inc"
+    include "debug.inc"
+
+; --- MAIN PROGRAM FILE ---
+init:
+; initialize play sample timer interrupt handler
+    call ps_prt_irq_init
+    call printNewLine
+    ret
+; end init
+
+main:
+; set a loop counter for multiple tests
+    ld b,10
+@start:
+    push bc
+; synchronise MOS timer with VBLANK
+    call vdu_vblank
+; set a MOS timer for 10 seconds
+    ld iy,tmr_test
+    ld hl,[120*10] ; 120 ticks per second
+    call tmr_set
+; start the PRT timer
+    call ps_prt_start
+@loop:
+; check MOS timer
+    call tmr_get
+    jp z,@F
+    jp m,@F
+    jp @loop
+@@: ; stop the PRT timer
+    call ps_prt_stop
+; display the interrupt count
+    ld hl,(ps_prt_irq_counter)
+    call printDec
+    call printNewLine
+; decrement the loop counter
+    pop bc
+    djnz @start
+    ret
+; end main
