0fb48f7f8b27b5065a70bc2abeff6251001436ac
bgates747 <brandon.gates@gmail.com>
2025-01-08 18:02:30 -0500

    fairly well behaved app with ascii art logo and fonts

---
+++ build/scripts/read_wav_header.py
@@ -0,0 +1,43 @@
+import struct
+
+def read_wav_header(file_path):
+    """Reads and parses the header of a WAV file according to the specified format."""
+    with open(file_path, "rb") as f:
+        header = f.read(44)  # WAV header is 44 bytes
+
+    # Parse fields from the header
+    riff = header[0:4].decode('ascii')
+    file_size = struct.unpack('<I', header[4:8])[0]  # Little-endian unsigned int
+    wave = header[8:12].decode('ascii')
+
+    fmt = header[12:16].decode('ascii')
+    fmt_chunk_size = struct.unpack('<I', header[16:20])[0]
+    audio_format = struct.unpack('<H', header[20:22])[0]  # 2-byte unsigned short
+    num_channels = struct.unpack('<H', header[22:24])[0]
+
+    sample_rate = struct.unpack('<I', header[24:28])[0]
+    byte_rate = struct.unpack('<I', header[28:32])[0]
+    block_align = struct.unpack('<H', header[32:34])[0]
+    bits_per_sample = struct.unpack('<H', header[34:36])[0]
+
+    data_marker = header[36:40].decode('ascii')
+    data_size = struct.unpack('<I', header[40:44])[0]
+
+    # Print results
+    print("RIFF Header:        ", riff)
+    print("File Size:          ", file_size)
+    print("WAVE Header:        ", wave)
+    print("Format Marker:      ", fmt)
+    print("Format Chunk Size:  ", fmt_chunk_size)
+    print("Audio Format:       ", audio_format, "(1 = PCM)")
+    print("Number of Channels: ", num_channels)
+    print("Sample Rate:        ", sample_rate)
+    print("Byte Rate:          ", byte_rate)
+    print("Block Align:        ", block_align)
+    print("Bits Per Sample:    ", bits_per_sample)
+    print("Data Marker:        ", data_marker)
+    print("Data Size:          ", data_size)
+
+# Test the function with the given file path
+file_path = "assets/sound/music/processed/Rhiannon.wav"
+read_wav_header(file_path)
--- src/asm/app.asm
+++ src/asm/app.asm
@@ -33,6 +33,8 @@ exit:
     include "arith24.inc"
     include "maths.inc"
     include "files.inc"
+    include "fonts.inc"
+    include "fonts_list.inc"
     include "fixed168.inc"
     include "timer.inc"
     include "vdu.inc"
@@ -42,6 +44,7 @@ exit:
     include "vdu_sound.inc"
 
 ; APPLICATION INCLUDES
+    include "ascii.inc"
     include "input.inc"
     include "music.inc"
     include "play.inc"
@@ -49,7 +52,46 @@ exit:
     include "debug.inc"
 
 ; --- MAIN PROGRAM FILE ---
+original_screen_mode: db 0
+
 init:
+; get current screen mode and save it so we can return to it on exit
+    call vdu_get_screen_mode
+    ld (original_screen_mode),a
+; set up display for gameplay
+    ld a,20
+    call vdu_set_screen_mode
+    xor a
+    call vdu_set_scaling
+; set text background color
+    ld a,c_blue_dk+128
+    call vdu_colour_text
+; set text foreground color
+    ld a,c_white
+    call vdu_colour_text
+; set the cursor off
+    call vdu_cursor_off
+; clear the screen
+    call vdu_cls
+; clear all buffers
+    call vdu_clear_all_buffers
+; load fonts
+	call fonts_load
+; select font
+    ld hl,Lat7_VGA8_8x8
+    ld a,1 ; flags
+    call vdu_font_select
+; print ascii art splash screen
+    call vdu_cls
+    ld c,0 ; x
+    ld b,4 ; y
+    call vdu_move_cursor
+    call printInline
+    asciz "Welcome to\r\n"
+    ld hl,agon_jukebox_ascii
+    call printString
+    call printInline
+    asciz "Press keys 0-9 to play a song...\r\n"
 ; load play sample command buffers
     call load_command_buffer
 ; initialize play sample timer interrupt handler
@@ -58,14 +100,28 @@ init:
 ; end init
 
 main:
-    call printNewLine
-; point to first song in the index
-    ld hl,SFX_filename_index
-    ld hl,(hl) ; pointer to first song filename
-    call play_song
+; ; point to first song in the index
+;     ld hl,SFX_filename_index
+;     ld hl,(hl) ; pointer to first song filename
+;     call play_song
+
+; call get_input to play first song
+    call get_input
+
+; shut down everytyhing and gracefully exit to MOS
     call ps_prt_stop ; stop the PRT timer
-    call printNewLine
     ei ; interrupts were disabled by input handler
+; restore original screen mode
+    ld a,(original_screen_mode)
+    call vdu_set_screen_mode
+    call vdu_reset_viewports
+    call vdu_cls
+; print thanks for playing message
+    call printInline
+    asciz "Thank you for using\r\n"
+    ld hl,agon_jukebox_ascii
+    call printString
+    call vdu_cursor_on
     ret ; back to MOS
 ; end main
 
