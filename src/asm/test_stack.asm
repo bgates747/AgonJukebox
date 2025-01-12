a51e5be00e10f20c280759655f625ecb08521cb4
bgates747 <brandon.gates@gmail.com>
2025-01-11 22:27:05 -0500

    bedtime save in working but incompletely updated state

---
+++ src/asm/test_stack.asm
@@ -0,0 +1,98 @@
+    assume adl=1 
+    org 0x040000 
+    jp start 
+    align 64 
+    db "MOS" 
+    db 00h 
+    db 01h 
+
+start: 
+    ; push af
+    ; push bc
+    ; push de
+    ; push ix
+    ; push iy
+
+    ; call init
+    ; call main
+
+    jp main
+
+exit:
+    ; pop iy
+    ; pop ix
+    ; pop de
+    ; pop bc
+    ; pop af
+    ld hl,0
+
+    ret
+
+; API INCLUDES
+    include "mos_api.inc"
+    include "macros.inc"
+    include "functions.inc"
+    include "arith24.inc"
+    include "maths.inc"
+    include "fonts.inc"
+    include "fonts_list.inc"
+    include "fixed168.inc"
+    include "timer.inc"
+    include "vdu.inc"
+    include "vdu_buffered_api.inc"
+    include "vdu_fonts.inc"
+    include "vdu_plot.inc"
+    include "vdu_sound.inc"
+
+; APPLICATION INCLUDES
+    include "layout.inc"
+    include "ascii.inc"
+    include "input_dir.inc"
+    include "play_dir.inc"
+    include "timer_jukebox.inc"
+    include "wav.inc"
+    include "debug.inc"
+
+; --- MAIN PROGRAM FILE ---
+original_screen_mode: db 0
+
+init:
+
+    ret
+; end init
+
+cmd_cd_music: asciz "cd music"
+cmd_cd_up: asciz "cd .."
+str_dashes: asciz "---------------------------------------------------------------"
+str_thick_dashes: asciz "==============================================================="
+
+main:
+    ld bc,0
+    ld (@stack_pointer),sp
+    ld hl,(@stack_pointer)
+    call printHexUHL
+    call printNewLine
+    ex de,hl
+@loop:
+    inc bc
+    push bc
+    ld (@stack_pointer),sp
+    ld hl,(@stack_pointer)
+    call printHexUHL
+    call printNewLine
+    or a ; clear carry
+    sbc hl,de
+    SIGN_HLU
+    jp nz,@loop
+    ld hl,(@stack_pointer)
+    call printHexUHL
+    push bc
+    pop hl
+    call printHexUHL
+    call printNewLine
+    call DEBUG_WAITKEYPRESS
+@stack_pointer: dl 0
+; end main
+
+; must be final include in program so file data does not stomp on program code or other data
+    include "files.inc"
