7564bf6762e39d45aa5a521c60707f85fba69beb
bgates747 <brandon.gates@gmail.com>
2025-01-17 22:06:18 -0500

    getting ready to implement ps_jukebox_struct

--- src/asm/app.asm
+++ src/asm/app.asm
@@ -26,6 +26,20 @@ exit:
 
     ret
 
+; JukeBox Structure (JB)
+;
+; Indexes into JukeBox structure
+jb_filehandle:          EQU 0          ;   1: File handle
+jb_file_idx:            EQU jb_filehandle+1  ;   1: Current file index in the directory page
+jb_filename:            EQU jb_file_idx+1    ;   3: Pointer to current file filename
+jb_dir_num_files:       EQU jb_filename+3    ;   3: Number of files/directories in the directory (virtually unlimited)
+jb_pagelast_num_files:  EQU jb_dir_num_files+3  ;   3: Mod(jb_dir_num_files, 10)
+jb_page_cur:            EQU jb_pagelast_num_files+3  ;   3: Current directory page number
+jb_dir_num_pages:       EQU jb_page_cur+3    ;   3: Number of pages in the directory (virtually unlimited)
+jb_filename_ptrs:       EQU jb_dir_num_pages+3  ;  30: List of filename pointers in the current directory page (10*3)
+jb_dir_path:            EQU jb_filename_ptrs+30 ; 256: Path of the current directory
+jb_struct_size:         EQU jb_dir_path+256  ; Total size of the JB structure
+
 ; API INCLUDES
     include "mos_api.inc"
     include "macros.inc"
