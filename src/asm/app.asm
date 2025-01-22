4d9e798da75397d2931a9b89c23753034d6751b8
bgates747 <brandon.gates@gmail.com>
2025-01-21 20:22:26 -0500

    song says it is playing, but there is no audio

--- src/asm/app.asm
+++ src/asm/app.asm
@@ -26,20 +26,6 @@ exit:
 
     ret
 
-; JukeBox Structure (JB)
-;
-; Indexes into JukeBox structure
-jb_filehandle:          EQU 0          ;   1: File handle
-jb_file_idx:            EQU jb_filehandle+1  ;   1: Current file index in the directory page
-jb_filename:            EQU jb_file_idx+1    ;   3: Pointer to current file filename
-jb_dir_num_files:       EQU jb_filename+3    ;   3: Number of files/directories in the directory (virtually unlimited)
-jb_pagelast_num_files:  EQU jb_dir_num_files+3  ;   3: Mod(jb_dir_num_files, bf_num_files_pg)
-jb_page_cur:            EQU jb_pagelast_num_files+3  ;   3: Current directory page number
-jb_dir_num_pages:       EQU jb_page_cur+3    ;   3: Number of pages in the directory (virtually unlimited)
-jb_filinfo_ptrs:       EQU jb_dir_num_pages+3  ;  30: List of filename pointers in the current directory page (bf_num_files_pg*3)
-jb_dir_path:            EQU jb_filinfo_ptrs+30 ; 256: Path of the current directory
-jb_struct_size:         EQU jb_dir_path+256  ; Total size of the JB structure
-
 ; API INCLUDES
     include "mos_api.inc"
     include "macros.inc"
