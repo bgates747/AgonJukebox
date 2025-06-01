bcb49daecdc19494069de970ff6ad981a6fe5485
bgates747 <brandon.gates@gmail.com>
2025-05-31 21:05:23 -0400

    implement audio limiter vdu functions and app controls

--- src/asm/test.asm
+++ src/asm/test.asm
@@ -42,21 +42,21 @@ exit:
     include "vdu_plot.inc"
     include "vdu_sound.inc"
 
-    include "fpp.inc"
-    include "fpp_ext.inc"
+;     include "fpp.inc"
+;     include "fpp_ext.inc"
 
-; APPLICATION INCLUDES
-    include "agm.inc"
-    include "layout.inc"
-    include "browse.inc"
-    include "input.inc"
-    include "logo.inc"
-    include "play.inc"
-    include "sort.inc"
-    include "timer_jukebox.inc"
-    include "wav.inc"
+; ; APPLICATION INCLUDES
+;     include "agm.inc"
+;     include "layout.inc"
+;     include "browse.inc"
+;     include "input.inc"
+;     include "logo.inc"
+;     include "play.inc"
+;     include "sort.inc"
+;     include "timer_jukebox.inc"
+;     include "wav.inc"
     
-    include "debug.inc"
+    ; include "debug.inc"
 
 test_buffer: dl 0xff1234
 ; --- MAIN PROGRAM FILE ---
@@ -66,34 +66,54 @@ init:
     ret
 ; end init
 main:
-    LD_HL_mn test_buffer
-    call printHex24
-    call printNewLine
+    MOSCALL mos_getkey ; a = ascii code of key pressed
+    cp '\e' ; escape
+    ret z 
 
-    LD_BC_mn test_buffer
-    push bc
-    pop hl
-    call printHex24
+    or a ; will be zero if function, alt, ctl, caps lock, or shift keys pressed
+    jr z,@check_fkeys
+    call printDec8
     call printNewLine
+    jr main
 
-    LD_DE_mn test_buffer
-    ex de,hl
-    call printHex24
-    call printNewLine
-
-    LD_IX_mn test_buffer
-    push ix
-    pop hl
-    call printHex24
-    call printNewLine
-
-    LD_IY_mn test_buffer
-    push iy
-    pop hl
-    call printHex24
-    call printNewLine
-
-    ret ; back to MOS
+@check_fkeys:
+    MOSCALL mos_getkbmap ; ix points to vdp-gl virtual keys map
+; 114 F1
+    bit 1,(ix+14)
+    jr z,@F
+    call printInline
+    asciz "F1\r\n"
+    jr main
+@@:
+; 115 F2
+    bit 2,(ix+14)
+    jr z,@F
+    call printInline
+    asciz "F2\r\n"
+    jr main
+@@:
+; 116 F3
+    bit 3,(ix+14)
+    jr z,@F
+    call printInline
+    asciz "F3\r\n"
+    jr main
+@@:
+; 21 F4
+    bit 4,(ix+2)
+    jr z,@F
+    call printInline
+    asciz "F4\r\n"
+    jr main
+@@:
+; 117 F5
+    bit 4,(ix+14)
+    jr z,@F
+    call printInline
+    asciz "F5\r\n"
+    jr main
+@@:
+    jr main
 ; end main
 
 ; must be final include in program so file data does not stomp on program code or other data
